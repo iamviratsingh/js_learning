f17768bce7411a59700ae907372524adc4963d2f
Virat <139986142+iamviratsingh@users.noreply.github.com>
2024-03-27 19:17:08 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=12345
+let accountEmail="virat@gmail.com"
+var accountPassword="123456"
+accountCity="Delhi"
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
