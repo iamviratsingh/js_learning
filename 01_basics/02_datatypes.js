154c501d66661c0bfb14b9108396fbceceefa25a
Virat <139986142+iamviratsingh@users.noreply.github.com>
2024-03-27 19:31:40 +0000

    datattypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "virat"
+let age = 25
+
+console.log(typeof "virat") // string
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
