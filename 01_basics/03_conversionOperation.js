724087dc9a6d1d7d769ae8c794ad2d68e9979742
Virat <139986142+iamviratsingh@users.noreply.github.com>
2024-03-28 19:03:16 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score=33
+let score1="33"
+let score2="33abc"
+let score3=null
+
+console.log(typeof score)
+console.log(typeof score1)
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber)
+
+
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+
+// when converted into boolean
+// 1 -> true; 0 -> false
+// "" -> false
+// "virat" -> true
